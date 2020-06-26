17fb200e34f97d2f77673e0ad8db09b873e75125
Juan <juan@pop-os.localdomain>
2020-06-26 22:40:26 +0200

    PF Finalizada

--- PFinal/src/PFinal.cpp
+++ PFinal/src/PFinal.cpp
@@ -201,7 +201,7 @@ Poblacion CRO(PAR estado, int num_datos, int datos_centro, int num_clusters, int
 			broadcast.erase(broadcast.begin()+p1);
 
 			int p2 = rand() % broadcast.size();
-			Poblacion padre2 = estado.devuelvePoblacion(broadcast[p1]);
+			Poblacion padre2 = estado.devuelvePoblacion(broadcast[p2]);
 			broadcast.erase(broadcast.begin()+p2);
 
 			Poblacion h1 = algoritmoCruceUN(padre1,padre2);
@@ -272,7 +272,7 @@ Poblacion CRO(PAR estado, int num_datos, int datos_centro, int num_clusters, int
 
 		Poblacion vacio(num_datos, datos_centro, num_clusters, min, max,restricciones,datos,lambda);
 		//Recorremos la población eliminando poblaciones en función de su salud
-		vector<int> p_sol = estado.peoresPadres(lambda,datos,restricciones,poblaciones*0.1);
+		vector<int> p_sol = estado.peoresPadres(lambda,datos,restricciones,poblaciones*0.2);
 		for(auto it_i=p_sol.begin(); it_i!=p_sol.end();it_i++){
 			int i= (*it_i);
 			float val_p = 0.5*generaciones;
@@ -299,10 +299,9 @@ Poblacion CRO(PAR estado, int num_datos, int datos_centro, int num_clusters, int
 		}
 		valoracion_ant = valoracion_mejor;
 
-		if(repeticion == 30){
+		if(repeticion == 20){
 			parar=true;
 		}
-		cout<<"Mejor sol: "<<valoracion_mejor<<endl;
 
 		evaluaciones++;
 		broadcast.clear();
@@ -368,7 +367,7 @@ Poblacion CRO_BL(PAR estado, int num_datos, int datos_centro, int num_clusters, 
 			broadcast.erase(broadcast.begin()+p1);
 
 			int p2 = rand() % broadcast.size();
-			Poblacion padre2 = estado.devuelvePoblacion(broadcast[p1]);
+			Poblacion padre2 = estado.devuelvePoblacion(broadcast[p2]);
 			broadcast.erase(broadcast.begin()+p2);
 
 			Poblacion h1 = algoritmoCruceUN(padre1,padre2);
@@ -414,7 +413,7 @@ Poblacion CRO_BL(PAR estado, int num_datos, int datos_centro, int num_clusters, 
 
 		//Se selecciona las mejores soluciones para que se reproduzcan
 
-		vector<int> m_corales = estado.mejoresPadres(lambda,datos,restricciones,10);
+		vector<int> m_corales = estado.mejoresPadres(lambda,datos,restricciones,2);
 		for(auto it_m = m_corales.begin(); it_m != m_corales.end(); it_m++){
 			intentos = 5+generaciones*0.1;
 			asentado = false;
@@ -437,10 +436,10 @@ Poblacion CRO_BL(PAR estado, int num_datos, int datos_centro, int num_clusters, 
 
 		Poblacion vacio(num_datos, datos_centro, num_clusters, min, max,restricciones,datos,lambda);
 		//Recorremos la población eliminando poblaciones en función de su salud
-		vector<int> p_sol = estado.peoresPadres(lambda,datos,restricciones,poblaciones*0.1);
+		vector<int> p_sol = estado.peoresPadres(lambda,datos,restricciones,poblaciones*0.3);
 		for(auto it_i=p_sol.begin(); it_i!=p_sol.end();it_i++){
 			int i= (*it_i);
-			float val_p = 0.1*generaciones;
+			float val_p = 0.5*generaciones;
 			int p_muerte = rand() % 100;
 			if(val_p > p_muerte){
 				estado.sustituyePoblacion(i,vacio);
@@ -456,13 +455,202 @@ Poblacion CRO_BL(PAR estado, int num_datos, int datos_centro, int num_clusters, 
 			}
 		}
 
-		if(generaciones % 10 == 0){
+		if(generaciones % 20 == 0){
 			estado.BLsuaveMejores(lambda,restricciones,datos,poblaciones*0.1);
 		}
 
 		int mejor_sol = estado.mejorCromosoma(lambda, datos, restricciones);
 		float valoracion_mejor = estado.devuelvePoblacion(mejor_sol).devolverValoracion();
-		cout<<"Mejor sol: "<<valoracion_mejor<<endl;
+
+		if(compara(valoracion_mejor,valoracion_ant)){
+			repeticion++;
+		}
+		else{
+			repeticion = 0;
+		}
+		valoracion_ant = valoracion_mejor;
+
+		if(repeticion == 20){
+			parar=true;
+		}
+
+		evaluaciones++;
+		broadcast.clear();
+		brooding.clear();
+		p_sol.clear();
+		m_corales.clear();
+		hijos.clear();
+	}
+
+	int mejor_sol = estado.mejorCromosoma(lambda,datos,restricciones);
+	Poblacion pob_mejor = estado.devuelvePoblacion(mejor_sol);
+	cout<<"Mejor solucion CRO-BL"<<endl;
+	int error = pob_mejor.calcularErrorGenerado();
+	cout<<pob_mejor.desviacionGeneral()<<"\t";
+	cout<<error<<"\t";
+	cout<<pob_mejor.devolverValoracion()<<"\t";
+	return pob_mejor;
+}
+
+Poblacion CRO_M(PAR estado, int num_datos, int datos_centro, int num_clusters, int poblaciones, int min, int max){
+	int rep_sexual;
+	int criterio_parada = 100000, evaluaciones = 0, generaciones = 0;
+	vector<int> corales;
+	vector<int> indices;
+	bool parar = false;
+	int repeticion=0;
+	float valoracion_ant = 0;
+	vector<int> broadcast;
+	vector<int> brooding;
+	vector<Poblacion> hijos;
+
+	for(int i =0; i<poblaciones;i++){
+		indices.push_back(i);
+	}
+
+	//Calculamos el número de espacios ocupados en este caso siempre vamos a generar el mismo número de corales, un 60% de las casillas están ocupadas al empezar por corales
+	int casillas_ocupadas = poblaciones*0.6;
+	//Para indicar si una casilla está inicializada simplemente comprobamos si alguno de sus datos está en el cluster -1.
+	for(int i=0; i<casillas_ocupadas;i++){
+		estado.asignacionAleatoria(i);
+		corales.push_back(i);
+	}
+
+
+	while(evaluaciones<criterio_parada && !parar){
+		rep_sexual = corales.size()*0.9;
+
+		//Seleccionamos los corales que generaran hijos de forma sexual
+		while(broadcast.size()<rep_sexual){
+			int dato_introducir = rand() % corales.size();
+			broadcast.push_back(corales[dato_introducir]);
+			corales.erase(corales.begin()+dato_introducir);
+		}
+		//y guardamos los que no para reproducción asexual
+		for(int i=0; i< corales.size(); i++){
+			brooding.push_back(corales[i]);
+		}
+
+		//seleccionamos las parejas de padres y creamos nuevos hijos
+		while(broadcast.size()>1){
+			int p1 = rand() % broadcast.size();
+			Poblacion padre1 = estado.devuelvePoblacion(broadcast[p1]);
+			broadcast.erase(broadcast.begin()+p1);
+
+			int p2 = rand() % broadcast.size();
+			Poblacion padre2 = estado.devuelvePoblacion(broadcast[p2]);
+			broadcast.erase(broadcast.begin()+p2);
+
+			Poblacion h1 = algoritmoCruceUN(padre1,padre2);
+			Poblacion h2 = algoritmoCruceUN(padre2,padre1);
+
+			hijos.push_back(h1);
+			hijos.push_back(h2);
+		}
+
+
+		while(brooding.size()>0){
+			int p = rand() % brooding.size();
+			Poblacion h2 = estado.devuelvePoblacion(brooding[p]);
+			h2.mutacionFuerte();
+			hijos.push_back(h2);
+			brooding.erase(brooding.begin()+p);
+		}
+
+		vector<int> m_padres = estado.mejoresPadres(lambda,datos,restricciones,poblaciones*0.3);
+			while(m_padres.size()>1){
+			int p1 = rand() % m_padres.size();
+			Poblacion padre1 = estado.devuelvePoblacion(m_padres[p1]);
+			m_padres.erase(m_padres.begin()+p1);
+
+			int p2 = rand() % m_padres.size();
+			Poblacion padre2 = estado.devuelvePoblacion(m_padres[p2]);
+			m_padres.erase(m_padres.begin()+p2);
+
+			Poblacion h1 = algoritmoCruceUN(padre1,padre2);
+			Poblacion h2 = algoritmoCruceUN(padre2,padre1);
+
+			hijos.push_back(h1);
+			hijos.push_back(h2);
+
+		}
+
+		int lugar, intentos;
+		bool asentado;
+		float val_orig, val_larva;
+
+		//Ahora con los hijos tratamos de introducirlos y guardamos el número máximo de intentos, en este caso 5.
+		for(auto it = hijos.begin(); it != hijos.end(); it++){
+			intentos = 5+generaciones*0.1;
+			asentado = false;
+			Poblacion larva = (*it);
+			while(intentos>0 && !asentado){
+				lugar = rand() % poblaciones;
+				val_orig = estado.devuelvePoblacion(lugar).devolverValoracion();
+				val_larva = larva.devolverValoracion();
+				if(val_larva<val_orig || -1==val_orig){
+					estado.sustituyePoblacion(lugar,larva);
+					asentado = true;
+					evaluaciones++;
+				}
+				else{
+					intentos--;
+				}
+			}
+		}
+
+
+		//Se selecciona las mejores soluciones para que se reproduzcan
+
+
+		vector<int> m_corales = estado.mejoresPadres(lambda,datos,restricciones,2);
+		for(auto it_m = m_corales.begin(); it_m != m_corales.end(); it_m++){
+			intentos = 5+generaciones*0.1;
+			asentado = false;
+			while(intentos>0 && !asentado){
+				lugar = rand() % poblaciones;
+				val_orig = estado.devuelvePoblacion(lugar).devolverValoracion();
+				val_larva = estado.devuelvePoblacion((*it_m)).devolverValoracion();
+				if(val_larva<val_orig || -1==val_orig){
+					estado.sustituyePoblacion(lugar,estado.devuelvePoblacion((*it_m)));
+					asentado = true;
+					evaluaciones++;
+				}
+				else{
+					intentos--;
+				}
+			}
+		}
+
+		//Introducimos la depredación en este caso se toma la mejor solución y cuando mayo sea la diferencia mayor probabilidad de morir
+
+		Poblacion vacio(num_datos, datos_centro, num_clusters, min, max,restricciones,datos,lambda);
+		//Recorremos la población eliminando poblaciones en función de su salud
+		vector<int> p_sol = estado.peoresPadres(lambda,datos,restricciones,poblaciones*0.2);
+		for(auto it_i=p_sol.begin(); it_i!=p_sol.end();it_i++){
+			int i= (*it_i);
+			float val_p = 0.5*generaciones;
+			int p_muerte = rand() % 100;
+			if(val_p > p_muerte){
+				estado.sustituyePoblacion(i,vacio);
+				evaluaciones++;
+			}
+		}
+
+		//Ahora comprobamos que casillas son corales y las añadimos al vector
+		corales.clear();
+		for(int i=0; i<poblaciones;i++){
+			if(0<(estado.devuelvePoblacion(i).devolverValoracion())){
+				corales.push_back(i);
+			}
+		}
+
+		if(generaciones % 20 == 0){
+			estado.BLsuaveMejores(lambda,restricciones,datos,poblaciones*0.1);
+		}
+
+		int mejor_sol = estado.mejorCromosoma(lambda, datos, restricciones);
+		float valoracion_mejor = estado.devuelvePoblacion(mejor_sol).devolverValoracion();
 
 		if(compara(valoracion_mejor,valoracion_ant)){
 			repeticion++;
@@ -486,7 +674,7 @@ Poblacion CRO_BL(PAR estado, int num_datos, int datos_centro, int num_clusters, 
 
 	int mejor_sol = estado.mejorCromosoma(lambda,datos,restricciones);
 	Poblacion pob_mejor = estado.devuelvePoblacion(mejor_sol);
-	cout<<"Mejor solucion CRO-BL"<<endl;
+	cout<<"Mejor solucion CRO-M"<<endl;
 	int error = pob_mejor.calcularErrorGenerado();
 	cout<<pob_mejor.desviacionGeneral()<<"\t";
 	cout<<error<<"\t";
@@ -529,7 +717,13 @@ int main(int argc, char **argv){
 		start_timers();
 		CRO_BL(problema2, numero_datos, datos_centro, clusters, numero_corales, minimo, maximo);
 		cout<<elapsed_time()<<endl;
+
 		cout<<"<--------------------------------------------------------------------------------------------->"<<endl;
 
+		start_timers();
+		CRO_M(problema2, numero_datos, datos_centro, clusters, numero_corales, minimo, maximo);
+		cout<<elapsed_time()<<endl;
+
+		cout<<"<--------------------------------------------------------------------------------------------->"<<endl;
 	}
 }
